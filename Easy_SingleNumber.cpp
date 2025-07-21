a69d9bd32afda56e688af03adb3f50f9c0a59b24
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-07-21 20:21:44 +0530

    Prob_1 Single Number (using ^(XOR) bitwise operator)

--- Easy_SingleNumber.cpp
+++ Easy_SingleNumber.cpp
@@ -5,24 +5,17 @@ using namespace std;
 class Solution {
     public:
     int singleNumber(vector<int>& nums) {
-        for(int num : nums) {
-            int result = 0;
-            for(int i = 0; i < nums.size(); i++) {
-                if(nums[i] == num) {
-                    result++;
-                }
-            }
-            if(result == 1) {
-                return num;
-            }
+        int result = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            result = result ^ nums[i];
         }
-        return 0;
+        return result;
     }
 };
 
 int main() {
     Solution s;
-    vector<int> nums = {2, 2, 1};
+    vector<int> nums = {4, 2, 1, 2, 1};
     cout << s.singleNumber(nums) << endl;
     return 0;
 }
